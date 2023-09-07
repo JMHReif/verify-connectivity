5ab7307512640c149ba90be1031307bc032d8d4a
Jennifer Reif <jennifer.reif@neo4j.com>
2023-09-06 22:17:27 -0500

    Initial commit

---
+++ src/main/java/com/jmhreif/verifyconnectivity/VerifyConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.jmhreif.verifyconnectivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyConnectivityApplication.class, args);
+	}
+
+}
