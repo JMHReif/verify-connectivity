64ce8efba0107d8d07cd9b4e131bf605cedc59a7
Jennifer Reif <jennifer.reif@neo4j.com>
2023-09-06 22:29:08 -0500

    Initial commit

--- src/main/java/com/jmhreif/verifyconnectivity/VerifyConnectivityApplication.java
+++ src/main/java/com/jmhreif/verifyconnectivity/VerifyConnectivityApplication.java
@@ -1,13 +1,27 @@
 package com.jmhreif.verifyconnectivity;
 
+import org.neo4j.driver.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// tag::option1[]
 @SpringBootApplication
-public class VerifyConnectivityApplication {
+public class VerifyConnectivityApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerifyConnectivityApplication.class, args);
 	}
 
+	final Driver driver;
+
+	public VerifyConnectivityApplication(@Autowired Driver driver) {
+		this.driver = driver;
+	}
+
+	public final void run(String... args) {
+		driver.verifyConnectivity();
+	}
 }
+// end::option1[]
--- src/test/java/com/jmhreif/verifyconnectivity/VerifyConnectivityApplicationTests.java
+++ src/test/java/com/jmhreif/verifyconnectivity/VerifyConnectivityApplicationTests.java
@@ -1,23 +1,13 @@
 package com.jmhreif.verifyconnectivity;
 
 import org.junit.jupiter.api.Test;
-import org.neo4j.driver.Driver;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class VerifyConnectivityApplicationTests {
 
-	// tag::option3[]
-	final Driver driver;
-	public VerifyConnectivityApplicationTests(@Autowired Driver driver) {
-		this.driver = driver;
-	}
-
 	@Test
-	final void testConnection() {
-		driver.verifyConnectivity();
+	void contextLoads() {
 	}
-	// end::option3[]
 
 }
