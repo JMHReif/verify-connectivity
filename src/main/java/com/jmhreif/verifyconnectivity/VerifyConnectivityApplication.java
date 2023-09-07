0e8c429559a33d288697588589b601337e248536
Jennifer Reif <jennifer.reif@neo4j.com>
2023-09-06 22:33:26 -0500

    Initial commit

--- src/main/java/com/jmhreif/verifyconnectivity/VerifyConnectivityApplication.java
+++ src/main/java/com/jmhreif/verifyconnectivity/VerifyConnectivityApplication.java
@@ -1,27 +1,13 @@
 package com.jmhreif.verifyconnectivity;
 
-import org.neo4j.driver.Driver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// tag::option1[]
 @SpringBootApplication
-public class VerifyConnectivityApplication implements CommandLineRunner {
+public class VerifyConnectivityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VerifyConnectivityApplication.class, args);
 	}
 
-	final Driver driver;
-
-	public VerifyConnectivityApplication(@Autowired Driver driver) {
-		this.driver = driver;
-	}
-
-	public final void run(String... args) {
-		driver.verifyConnectivity();
-	}
 }
-// end::option1[]
